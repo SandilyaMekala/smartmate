affdb5372c84d6a95cca4c61c3792181fbc99121
SandilyaMekala <sandilyamekala@gmail.com>
2024-08-13 13:34:07 -0500

    CSS changes

--- src/UserActivities/UserActivities.js
+++ src/UserActivities/UserActivities.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import { useLocation } from "react-router-dom";
+import "./UserActivities.css";
 
-const UserActivities = (props) => {
+const UserActivities = () => {
     const location = useLocation();
     const [userActivities, setUserActivities] = useState([])
 
@@ -10,11 +11,11 @@ const UserActivities = (props) => {
         axios.get(`https://jsonplaceholder.typicode.com/posts/?userId=${location.state.userId}`)
             .then((response) => {setUserActivities(response.data)})
             .catch((err) => console.log(err))
-    }, []);
+    }, [location.state.userId]);
 
     return (
-        <div>{userActivities.map((i) => 
-            <div>
+        <div className="user-activities">{userActivities.map((i) => 
+            <div key={i.id}>
                 <label>Title</label>
                 <p>{i.title}</p>
                 <label>Post</label>
--- src/UserProfile/UserProfile.js
+++ src/UserProfile/UserProfile.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import { Link, useLocation } from "react-router-dom";
+import "./UserProfile.css";
 
 const UserProfile = () => {
     const location = useLocation();
@@ -9,23 +10,26 @@ const UserProfile = () => {
     useEffect(() => {
         axios.get(`https://jsonplaceholder.typicode.com/users/${location.state.userId}`)
             .then((response) => {setUserData(response.data)})
-            .catch((err) => console.log(err))
-    }, []);
+            .catch((err) => {console.log(err); setUserData(null)})
+    }, [location.state.userId]);
 
     return (
         <div>{
             userData !== null || undefined ?
-                <div>
-                    <label>Name:</label>
+                <div className="user-profile">
+                    <label>Name</label>
                     <p>{userData.name}</p>
-                    <label>Email:</label>
+                    <label>Email</label>
                     <p>{userData.email}</p>
-                    <label>Phone:</label>
+                    <label>Phone</label>
                     <p>{userData.phone}</p>
+                    <Link to='/userActivities' state={{userId: location.state.userId}}>Click to view User Activities</Link>
                 </div>
-            : null}
-            <Link to='/userActivities' state={{userId: location.state.userId}}>Activities</Link>
-        </div>
+            : <div style={{textAlign: 'center'}}>
+                <p style={{fontSize: '24px'}}>No user found</p>
+                <Link to='/'>Go back</Link>
+            </div>
+        }</div>
     );
 }
 
